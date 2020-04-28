d3a66550ed8c6216b13002ce6e5b425367f30770
DenioD <41270280+DenioD@users.noreply.github.com>
2020-04-28 23:25:44 +0200

    remove check for duplicated addresses for our Headermemo

--- lib/src/lightwallet.rs
+++ lib/src/lightwallet.rs
@@ -1453,16 +1453,16 @@ impl LightWallet {
             return Err("Need at least one destination address".to_string());
         }
 
-        // Check for duplicates in the to list
-        if tos.len() > 1 {
-            let mut to_addresses = tos.iter().map(|t| t.0.to_string()).collect::<Vec<_>>();
-            to_addresses.sort();
-            for i in 0..to_addresses.len()-1 {
-                if to_addresses[i] == to_addresses[i+1] {
-                    return Err(format!("To address {} is duplicated", to_addresses[i]));
-                }
-            }
-        }
+        // Check for duplicates in the to list - We need that for HushChat
+       // if tos.len() > 1 {
+       //     let mut to_addresses = tos.iter().map(|t| t.0.to_string()).collect::<Vec<_>>();
+       //     to_addresses.sort();
+       //     for i in 0..to_addresses.len()-1 {
+       //         if to_addresses[i] == to_addresses[i+1] {
+       //             return Err(format!("To address {} is duplicated", to_addresses[i]));
+       //         }
+       //     }
+       // }
 
         let total_value = tos.iter().map(|to| to.1).sum::<u64>() as u64;
         println!(
